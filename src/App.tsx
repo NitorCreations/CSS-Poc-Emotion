72cd9aae8c71317cb3cb061319f781642b1bd43b
Annina Kivikari <annina.kivikari@nitor.com>
2022-09-16 10:05:02 +0300

    fixed formic and added emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,6 @@
-<<<<<<< Updated upstream
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { Formik, Field, Form, FormikHelpers } from "formik";
+import { Formik, Field, Form, FormikHelpers } from 'formik';
 
 interface Values {
   firstName: string;
@@ -38,9 +14,9 @@ function App() {
       <h1>Signup</h1>
       <Formik
         initialValues={{
-          firstName: "",
-          lastName: "",
-          email: "",
+          firstName: '',
+          lastName: '',
+          email: '',
         }}
         onSubmit={(
           values: Values,
@@ -53,24 +29,23 @@ function App() {
         }}
       >
         <Form>
-          <label htmlFor="firstName">First Name</label>
-          <Field id="firstName" name="firstName" placeholder="John" />
+          <label htmlFor='firstName'>First Name</label>
+          <Field id='firstName' name='firstName' placeholder='John' />
 
-          <label htmlFor="lastName">Last Name</label>
-          <Field id="lastName" name="lastName" placeholder="Doe" />
+          <label htmlFor='lastName'>Last Name</label>
+          <Field id='lastName' name='lastName' placeholder='Doe' />
 
-          <label htmlFor="email">Email</label>
+          <label htmlFor='email'>Email</label>
           <Field
-            id="email"
-            name="email"
-            placeholder="john@acme.com"
-            type="email"
+            id='email'
+            name='email'
+            placeholder='john@acme.com'
+            type='email'
           />
 
-          <button type="submit">Submit</button>
+          <button type='submit'>Submit</button>
         </Form>
       </Formik>
->>>>>>> Stashed changes
     </div>
   );
 }
